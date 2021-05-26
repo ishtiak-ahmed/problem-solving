ccd9a5a5485c0aa1ff9baa59010d5922ce600dd4
Ishtiak Ahmed <57805696+ishtiak-ahmed@users.noreply.github.com>
2021-05-26 22:44:24 +0600

    Update sringReduction.js

--- sringReduction.js
+++ sringReduction.js
@@ -37,33 +37,36 @@ function reducer(ab){
 
 function checkStr(str){
   const length = str.length;
-  for(let i = 0; i < length-1; i++){
-      if(str[i] === str[1+1]){
-        return true;
+  let result = true;
+  for(let i = 0; i < (length-1); i++){
+      if(str[i] == str[i+1]){
+        continue;
       }else {
-        return false
+        result = false
       }
     }
+    return result;
 }
 
 function StringReduction(str) { 
 
   // code goes here  
   if(checkStr(str)){
-    console.log('cant reduce')
     return str.length
   }else {
-  //     let ab = str.slice(0,2)
-  // if(checkStr(ab)){
-  //   ab = str.slice(1,3)
-  //   const c = reducer(ab)
-  //   const newStr = str.replace(ab, c)
-  //   StringReduction(newStr)
-  // }
+    let ab = str.slice(0,2)
+    if(checkStr(ab)){
+      ab = str.slice(1,3)
+      console.log(ab)
+    }
+      const c = reducer(ab)
+      const newStr = str.replace(ab, c)
+      console.log(newStr)
+      StringReduction(newStr)
   }
-
 }
    
 // keep this function call here 
 // @ts-ignore
 console.log(StringReduction(readline()));
+// console.log(checkStr(readline()));
